6fd05f04cf0b0b09f04e774f1d84cd0cf9523de7
gotom <chernogorov_m@mail.ru>
2021-04-15 14:39:45 +0500

    Добавлены спрайты и отрисовка

--- src/main/java/worldObjects/WorldGameObject.java
+++ src/main/java/worldObjects/WorldGameObject.java
@@ -2,6 +2,10 @@ package worldObjects;
 
 import graphics.DrawPriorities;
 
+import java.awt.image.BufferedImage;
+
 public interface WorldGameObject {
     DrawPriorities getPriority();
+    BufferedImage getSpriteSheet();
+    void changeSprite();
 }
